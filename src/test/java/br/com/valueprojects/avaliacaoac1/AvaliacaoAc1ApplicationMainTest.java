5bc5e7375ac4d229d30600a7d591946f098db885
Leonardo Kuntz <99610937+scuiki@users.noreply.github.com>
2025-11-18 17:32:25 -0300

    correção de teste.

--- src/test/java/br/com/valueprojects/avaliacaoac1/AvaliacaoAc1ApplicationMainTest.java
+++ src/test/java/br/com/valueprojects/avaliacaoac1/AvaliacaoAc1ApplicationMainTest.java
@@ -1,11 +1,28 @@
 package br.com.valueprojects.avaliacaoac1;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 
 class AvaliacaoAc1ApplicationMainTest {
 
     @Test
-    void mainDeveSubirAplicacaoSemErro() {
-        AvaliacaoAc1Application.main(new String[]{});
+    void mainDeveChamarSpringApplicationRun() {
+        try (MockedStatic<SpringApplication> springAppMock = Mockito.mockStatic(SpringApplication.class)) {
+
+            springAppMock
+                .when(() -> SpringApplication.run(eq(AvaliacaoAc1Application.class), any(String[].class)))
+                .thenReturn(null);
+
+            AvaliacaoAc1Application.main(new String[]{});
+
+            springAppMock.verify(
+                () -> SpringApplication.run(eq(AvaliacaoAc1Application.class), any(String[].class))
+            );
+        }
     }
 }
